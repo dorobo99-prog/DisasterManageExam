faa4f500dd6eb153dad349a8b3106080fec6e424
dorobo99-prog <dorobo99@gmail.com>
2026-04-19 19:05:24 +0900

    Delete server directory

--- server/api/check_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/_session.php';
-header('Content-Type: application/json; charset=utf-8');
-
-if (!empty($_SESSION['authenticated'])) {
-    echo json_encode(array('ok' => true, 'name' => $_SESSION['name']));
-} else {
-    echo json_encode(array('ok' => false));
-}
--- server/api/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/_session.php';
-header('Content-Type: application/json; charset=utf-8');
-
-session_destroy();
-setcookie(SESSION_NAME, '', time() - 3600, '/');
-
-echo json_encode(array('ok' => true));
